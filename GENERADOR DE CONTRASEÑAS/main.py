b090c87c733631be05296073e3a554b527652a57
Aleccito <alexanderxrv07@gmail.com>
2025-08-27 18:48:37 -0500

    Update main.py
    Agregando validaciones

--- GENERADOR DE CONTRASEÑAS/main.py
+++ GENERADOR DE CONTRASEÑAS/main.py
@@ -2,19 +2,34 @@ from utilidades import *
 
 def main():
     print("\t\tVERIFICADOR / GENERADOR DE CONTRASEÑAS")
-    print("\n1. Generar contraseña automatica")
-    print("2. Verificar mi fuerza de mi contraseña")
-    
-    opcion = int(input("Ingrese una opcion: "))
+    while True:
+        print("\n1. Generar contraseña automática")
+        print("2. Verificar la fuerza de mi contraseña")
+        opcion = input("\nIngrese una opción (1, 2): ")
+        print("-------------------------------------")
 
-    if opcion == 1:
-        print(f"Tu contraseña es: {generar_contraseña()} ")
-        
-    if opcion == 2:
-        contraseña = input("Ingrese contraseña: ")
-        puntuacion, nivel, entropia = fuerza_contraseña(contraseña)
-        print(f"Contraseña: {contraseña}\nPuntuación: {puntuacion}%\nNivel: {nivel}\nEntropía: {entropia} bits")
+        try:
+            opcion = int(opcion)
+            
+            if opcion == 1:
+                print(f"Tu contraseña es: {generar_contraseña()}") 
+                print("-------------------------------------")       
+                         
+            elif opcion == 2:
+                contraseña = input("Ingrese contraseña: ")
+                puntuacion, nivel, entropia = fuerza_contraseña(contraseña)
+                print(f"Contraseña: {contraseña}\nPuntuación: {puntuacion}%\nNivel: {nivel}\nEntropía: {entropia} bits")
+                print("-------------------------------------")  
+            else:
+                print("Opción no válida (1, 2).")
+                print("-------------------------------------")
+
+        except Exception as x:
+            print("\nIngresa una opción correcta. ")
+            print(f"ERROR: {type(x).__name__}")
+            print("-------------------------------------")
     
 
 if __name__ == "__main__":
+
     main()
